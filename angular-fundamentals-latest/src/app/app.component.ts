8007f71cbe649d937de24bbdbd7d0c4d817496f4
pedrocampos <pedro.campos@startaideia.com.br>
2023-12-04 11:39:05 -0300

    feat: add styles css explain

---
+++ angular-fundamentals-latest/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './components/new-component/new-component.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, NewComponent],
+  template: `
+    <router-outlet></router-outlet>
+    <app-new-component />
+  `,
+})
+export class AppComponent {}
