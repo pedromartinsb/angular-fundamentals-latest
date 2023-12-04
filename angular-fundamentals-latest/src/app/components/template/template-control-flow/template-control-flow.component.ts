42df2bb3471876a1698c96cb53aa8c258badf3f0
pedrocampos <pedro.campos@startaideia.com.br>
2023-12-04 16:04:57 -0300

    feat: add @for

--- angular-fundamentals-latest/src/app/components/template/template-control-flow/template-control-flow.component.ts
+++ angular-fundamentals-latest/src/app/components/template/template-control-flow/template-control-flow.component.ts
@@ -1,13 +1,25 @@
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { Observable, delay, of } from 'rxjs';
 
 @Component({
   selector: 'app-template-control-flow',
   standalone: true,
-  imports: [CommonModule],
+  imports: [AsyncPipe, NgIf, NgFor],
   templateUrl: './template-control-flow.component.html',
-  styleUrl: './template-control-flow.component.scss'
+  styleUrl: './template-control-flow.component.scss',
 })
 export class TemplateControlFlowComponent {
   public isTrue = true;
+
+  public items = [{ name: 'Pedro Campos' }];
+  public loadingData$: Observable<string[]> = of([
+    'item 1',
+    'item 2',
+    'item 3',
+  ]).pipe(delay(3000));
+
+  public trackByFn(index: number) {
+    return index;
+  }
 }
